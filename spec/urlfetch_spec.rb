df76ebcfb3ddfe3376271f3269267d358451a0f6
Tobias <tobias@LittleGeek.local>
2010-12-22 02:01:30 +0100

    Using RSpec.

--- spec/urlfetch_spec.rb
+++ spec/urlfetch_spec.rb
@@ -1,6 +1,11 @@
+require 'urlfetch'
 
-require File.join(File.dirname(__FILE__), %w[spec_helper])
 
 describe Urlfetch do
+  describe "#pack" do
+    it "packs binary data" do
+      Urlfetch.pack(3, "foo").should == "\003foo"
+      Urlfetch.pack(3, "foo", 4, "test").should == "\003foo\004test"
+    end
+  end
 end
-
