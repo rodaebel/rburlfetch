aa2dc55faafc8d985ae8691faa5f51ba18542492
Tobias <tobias@LittleGeek.local>
2010-12-22 03:02:33 +0100

    Added another behaviour.

--- spec/urlfetch_spec.rb
+++ spec/urlfetch_spec.rb
@@ -2,10 +2,20 @@ require 'urlfetch'
 
 
 describe Urlfetch do
+
   describe "#pack" do
     it "packs binary data" do
       Urlfetch.pack(3, "foo").should == "\003foo"
       Urlfetch.pack(3, "foo", 4, "test").should == "\003foo\004test"
     end
   end
+
+  describe "#command" do
+    it "encodes a command sequence" do
+      Urlfetch.command("foo").should == "foo"
+      Urlfetch.command("foo", "bar").should == "\003foobar"
+      Urlfetch.command("foo", "test", "bar").should == "\003foo\004testbar"
+    end
+  end
+
 end
